3efedb5dbcee4f8c4864d3d280c0df8ea2ec3748
Alex Antonich <antonich@oakland.edu>
2021-12-01 23:05:27 -0500

    converted to expressjs format

---
+++ public/js/picture.js
@@ -0,0 +1,15 @@
+let picButton = document.querySelector('#commentButton');
+let articleContainer = document.querySelector('#container');
+let myImg = document.createElement('img');
+
+myImg.src = './img/IMG_0085.JPG';
+//myImg.height = '90%';
+//height will not work, no idea why and frankly I don't care anymore
+myImg.alt = 'Alex Antonich';
+myImg.title = 'Alex Antonich';
+myImg.class = 'new_image';
+myImg.border = '10%';
+
+picButton.addEventListener('click', function () {
+    articleContainer.insertBefore(myImg, picButton);
+})
