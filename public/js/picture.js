0f49bf6c831272a25a0a8ce9470c8a474be106d0
Alex Antonich <antonich@oakland.edu>
2021-12-01 23:24:55 -0500

    fixed img

--- public/js/picture.js
+++ public/js/picture.js
@@ -2,7 +2,7 @@ let picButton = document.querySelector('#commentButton');
 let articleContainer = document.querySelector('#container');
 let myImg = document.createElement('img');
 
-myImg.src = '../public/img/1200px-Oakland_Golden_Grizzlies_logo.svg.png';
+myImg.src = '../public/img/IMG_0085.JPG';
 //myImg.height = '90%';
 //height will not work, no idea why and frankly I don't care anymore
 myImg.alt = 'Alex Antonich';
