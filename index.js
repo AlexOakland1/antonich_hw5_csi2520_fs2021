eaa01644e7a2337a23cb6098ac0ee3a7d4949260
Alex Antonich <antonich@oakland.edu>
2021-12-03 18:55:52 -0500

    attempting server recieve and response

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/updatescore", (req, res) => {
     if (check == "1") {   
         res.render('<p>some html</p>')
     }else{
-        console.log(check);
+        res.render('<p>no bueno</p>')
     };
 });
 
