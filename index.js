d4e4ad1bfd8a640ab2fa4b3dc5ae00b81b6f3823
Alex Antonich <antonich@oakland.edu>
2021-12-03 17:36:45 -0500

    attempting server recieve and response

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/my_resume", (req, res) => {
 });
 
 app.get("/updatescore", (req, res) => {
-    let check = req.body.fav_language;
+    let check = req.body.fav_language.value;
     res.send(check);
 });
 
