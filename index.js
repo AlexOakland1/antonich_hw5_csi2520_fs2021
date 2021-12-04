4ba641d476ec20a38970414ae90d3a52064180a7
Alex Antonich <antonich@oakland.edu>
2021-12-03 23:58:04 -0500

    attempting server recieve and response

--- index.js
+++ index.js
@@ -34,10 +34,46 @@ app.get("/triviaResult", (req, res) => {
 });
 
 app.post("/updatescore", (req, res) => {
-    //let { check } = req.query;
-    let test = [req.body.q1, req.body.q2];
-    //let check = req.get('Content-Type');
-    res.render("triviaResult", {data: test});
+    var score = 0;
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    let d = 0;
+    let e = 0;
+    let q1 = req.body.q1.trim();
+    let q2 = req.body.q2.trim();
+    let q3 = req.body.q3.trim();
+    let q4 = req.body.q4.trim();
+    let q5 = req.body.q5.trim();
+
+    if (q1 == 3){
+        score += 1;
+        a = 1;
+    };
+    if (q2 == 3){
+        score += 1;
+        b = 1;
+    };
+    if (q3 == 1){
+        score += 1;
+        c = 1;
+    };
+    if (q4 == 2){
+        score += 1;
+        d = 1;
+    };
+    if (q5 == 3){
+        score += 1;
+        e = 1;
+    };
+    console.log(score);
+    res.render("triviaResult", {data1: a,
+    data2: b,
+    data3: c,
+    data4: d,
+    data5: e,
+    score: score
+    });
 });
 
 // Setup server ports
