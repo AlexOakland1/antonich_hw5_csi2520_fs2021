ecfd0f0eb8b686e5ffdeff41944aad9aaee27fc1
Alex Antonich <antonich@oakland.edu>
2021-12-03 19:46:19 -0500

    attempting server recieve and response

--- index.js
+++ index.js
@@ -31,10 +31,11 @@ app.get("/my_resume", (req, res) => {
 
 app.get("/updatescore", (req, res) => {
     let check = req.body.q1;
+    res.render("triviaResult", {data: check})
     if (check == "1") {   
         res.render('<p>some html</p>')
     }else{
-        res.render('<p>no bueno</p>')
+        res.render('<p>wrong</p>')
     };
 });
 
